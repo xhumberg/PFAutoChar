95ff8af9c72e07d9e0f8d5f86782736724d2a29f
humberg <humberg@lab1-14.eng.utah.edu>
2018-04-30 01:56:06 -0600

    Perceptron layer added.

--- DecisionTree.py
+++ DecisionTree.py
@@ -8,6 +8,9 @@ import csv
 import os
 
 from sklearn import tree
+from sklearn.linear_model import perceptron
+
+debug = False
 
 def loadInputs(fname):
     retvalues = []
@@ -46,6 +49,34 @@ def DecisionTree(vals, treeNum):
     retval = retval.fit(parameters, labels)
     return retval
 
+def GenPerceptronLayer(vals):
+	nets = {}
+	trainingData = {}
+	trainingValues = {}
+	for i in range (0, 36):
+		indx = i+1
+		data = [0]*12;
+		for j in range(0, 11):
+			data[j] = float(vals[indx][j])
+		for j in [13, 15, 17]:
+			if vals[indx][j] not in trainingData:
+				trainingData[vals[indx][j]] = []
+				trainingValues[vals[indx][j]] = []
+			trainingData[vals[indx][j]].append(data);
+			if vals[indx][j+1] is "1":
+				trainingValues[vals[indx][j]].append(1);
+			else:
+				trainingValues[vals[indx][j]].append(0);
+	for string in trainingData:
+		if 1 not in trainingValues[string] or 0 not in trainingValues[string]:
+			print "-E- Only one label exists for " + string + ": " + str(trainingValues[string]);
+			continue
+		nets[str(string)] = perceptron.Perceptron(max_iter=100, tol=None, verbose=0, random_state=None, fit_intercept=True, eta0=0.002)
+		nets[str(string)].fit(trainingData[string], trainingValues[string]);
+		if debug is True:
+			print string + ": " + str(nets[string].score(trainingData[string], trainingValues[string])*100) + "% testing accuracy"
+	return nets;
+
 ##change current working directory, needed for my computer
 #os.chdir(r'F:\school\Nate\cs5350\project\PFAutoChar')
 values = loadInputs("percepTrain.csv")
@@ -53,6 +84,26 @@ values = loadInputs("percepTrain.csv")
 aggressiveTree = DecisionTree(values, 1)
 middleTree = DecisionTree(values, 2)
 passiveTree = DecisionTree(values, 3)
+
+perceptrons = GenPerceptronLayer(values);
+
+testArray = [[3,0,0,1,1,1,2,1,2,0.18,0.57,3]]
+prediction1 = str(aggressiveTree.predict(testArray)[0]);
+prediction2 = str(middleTree.predict(testArray)[0]);
+prediction3 = str(passiveTree.predict(testArray)[0]);
+net = perceptrons[prediction1]
+if debug is True:
+	print "Aggressively predicted: " + prediction1 + ". Perceptron says : " + str(perceptrons[prediction1].predict(testArray)[0]);
+	print "Progressively predicted: " + prediction2 + ". Perceptron says : " + str(perceptrons[prediction2].predict(testArray)[0]);
+	print "Defensively predicted: " + prediction3 + ". Perceptron says : " + str(perceptrons[prediction3].predict(testArray)[0]);
+
+result = prediction3
+if str(perceptrons[prediction1].predict(testArray)[0]) is "1":
+	result = prediction1
+elif str(perceptrons[prediction2].predict(testArray)[0]) is "1":
+	result = prediction2
+
+print "You should " + result
 
 ##code testing
 
